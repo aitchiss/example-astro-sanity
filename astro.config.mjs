6450bc8c5f9e1df07e48fe6932d1d29c6f85e351
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2024-06-03 14:47:36 +0100

    use studio path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
-import { defineConfig } from 'astro/config';
-import sanityIntegration from '@sanity/astro';
-import react from '@astrojs/react';
-import { loadEnv } from 'vite';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import sanityIntegration from "@sanity/astro";
+import react from "@astrojs/react";
+import { loadEnv } from "vite";
+import netlify from "@astrojs/netlify";
 
-const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } = loadEnv(
-  process.env.NODE_ENV,
-  process.cwd(),
-  ''
-);
+const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } =
+  loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
-  output: 'hybrid',
+  output: "hybrid",
   integrations: [
     sanityIntegration({
       projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
       dataset: PUBLIC_SANITY_STUDIO_DATASET,
       useCdn: false,
-      studioBasePath: '/admin',
+      studioBasePath: "/studio",
     }),
     react(),
   ],
